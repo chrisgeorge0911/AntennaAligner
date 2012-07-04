2c4f23d851feba1aaacdefbe2720cbd5a90bfd33
Chris <Chris@34b55c8c-a26e-5a4a-9a6d-ceb4c9c93e45>
2012-07-04 20:56:39 +0000

    Some code tidy
    git-svn-id: svn://localhost/antennaaligner@48 34b55c8c-a26e-5a4a-9a6d-ceb4c9c93e45

--- trunk/NomadProject1/scripts/positionUtils.js
+++ trunk/NomadProject1/scripts/positionUtils.js
@@ -1,11 +1,10 @@
-﻿Number.prototype.toDeg = function () {
+﻿Number.prototype.toDeg = function() {
     return this * 180 / Math.PI;
-}
+};
 
-Number.prototype.toRad = function () {
+Number.prototype.toRad = function() {
     return this * Math.PI / 180;
-}
-
+};
 
 function getBearing(pos1, pos2) {
     var lat1 = pos1.coords.latitude;
@@ -36,11 +35,11 @@ function getDistanceBetween(pos1, pos2) {
     var R = 6371; // km
     var dLat = (lat2 - lat1).toRad();
     var dLon = (lon2 - lon1).toRad();
-    var lat1 = lat1.toRad();
-    var lat2 = lat2.toRad();
+    var lat1Rad = lat1.toRad();
+    var lat2Rad = lat2.toRad();
 
     var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-            Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+            Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1Rad) * Math.cos(lat2Rad);
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
     var d = R * c;
 
