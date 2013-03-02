b8646380dfb199bb589bcb641e9341933ccafb13
Chris George <chris@chrisgeorge.org.uk>
2013-03-02 21:35:44 +0000

    Give weight to low powered/close transmitters to try and compemsate for lack of terrain data

--- trunk/NomadProject1/scripts/signal.js
+++ trunk/NomadProject1/scripts/signal.js
@@ -37,6 +37,13 @@ function getListOfTxBySignalStrength(lastPosition) {
     for (var i = 0; i < txList.length; i++) {
         txList[i].distance = getDistanceBetween(lastPosition, txList[i].position);
         txList[i].signalStrength = getFieldStrength(txList[i].channel[0].pwr, txList[i].distance, txList[i].channel[0].ch);
+
+        if (txList[i].distance < 10 && txList[i].channel[0].pwr < 500)
+            txList[i].signalStrength = txList[i].signalStrength * 1.2;
+        else if (txList[i].distance < 3 && txList[i].channel[0].pwr < 50)
+            txList[i].signalStrength = txList[i].signalStrength * 1.5;
+        else if (txList[i].distance < 2 && txList[i].channel[0].pwr < 50)
+            txList[i].signalStrength = txList[i].signalStrength * 1.7;
     }
 
     txList.sort(signalSort);
@@ -46,24 +53,26 @@ function getListOfTxBySignalStrength(lastPosition) {
 
 function getNearestTx(position) {
 
+    var numTx = 20;
+
     var txList = getListOfTxBySignalStrength(position);
 
-    var txListTop5 = new Array();
+    var txListTopN = new Array();
 
-    var top5ListCount = 0;
+    var topNListCount = 0;
     // get the top 5 transmitters that have signal strength 66db+
     for (var i = 0; i < txList.length; i++) {
         var currentTx = txList[i];
 
         if (currentTx.distance < c_DistanceLimit) {
-            txListTop5[top5ListCount] = txList[i];
-            txListTop5[top5ListCount].distanceMiles = txListTop5[top5ListCount].distance * 0.62137119;
-            top5ListCount++;
+            txListTopN[topNListCount] = txList[i];
+            txListTopN[topNListCount].distanceMiles = txListTopN[topNListCount].distance * 0.62137119;
+            topNListCount++;
         }
 
-        if (txListTop5.length == 20)
+        if (txListTopN.length == numTx)
             break;
     }
 
-    return txListTop5;
+    return txListTopN;
 }
